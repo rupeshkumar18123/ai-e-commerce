0756bc900c2b7404a04a57c1bf5ad4e5576aa940
Rupesh kumar <rupesh18123143@gmail.com>
2025-07-26 08:57:45 +0530

    add api

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { processQuery } = require('../controllers/chatController');
 const { register, login, getProfile } = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
-
+//adding api
 router.post('/process', processQuery);
 router.post('/auth/register', register);
 router.post('/auth/login', login);
